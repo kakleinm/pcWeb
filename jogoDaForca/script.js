f2547eb459f3e0a85180272656e5a240c92be8f5
Karen KM <karenkmaia09@gmail.com>
2025-05-10 22:05:59 -0300

    add: letras erradas

--- jogoDaForca/script.js
+++ jogoDaForca/script.js
@@ -14,19 +14,23 @@ const img = document.querySelector("#img")
 
 let tentativas = 6
 let venceu = 0
+let letrasErradas = []
+let letraRepetida
 
-botao.addEventListener("click", function () {
-  const letra = document.querySelector("#letra").value.toUpperCase()
-  let letraCorreta = false
-    for (let i = 0; i < palavraSorteada.length; i++) {
-    if (letra == palavraSorteada[i] && arrayPalavra[i] == "_") {
-      arrayPalavra[i] = letra
-      letraCorreta = true
-      venceu++
+const verificaRepeticao = (letra) => {
+  letraRepetida = false
+  for (let i = 0; i < letrasErradas.length; i++) {
+    if (letrasErradas[i] === letra) {
+      letraRepetida = true
+      break
     }
   }
-  resposta.innerHTML = arrayPalavra + "<br> Tentativas = " + tentativas
-  if (!letraCorreta) tentativas--
+  if (!letraRepetida) {
+    letrasErradas.push(letra)
+  }
+}
+
+const exibeImagem = () => {
   switch(tentativas) {
     case 5 : img.src="../jogoDaForca/img/2.jpg"; break
     case 4 : img.src="../jogoDaForca/img/3.jpg"; break
@@ -35,6 +39,25 @@ botao.addEventListener("click", function () {
     case 1 : img.src="../jogoDaForca/img/6.jpg"; break
     case 0 : img.src="../jogoDaForca/img/7.jpg"; break
   } 
+}
+
+botao.addEventListener("click", function () {
+  const letra = document.querySelector("#letra").value.toUpperCase()
+  let letraCorreta = false
+  for (let i = 0; i < palavraSorteada.length; i++) {
+    if (letra == palavraSorteada[i] && arrayPalavra[i] == "_") {
+      arrayPalavra[i] = letra
+      letraCorreta = true
+      venceu++
+    }
+  }
+  if (!letraCorreta) {
+    verificaRepeticao(letra)
+    tentativas--
+  }
+
+  resposta.innerHTML = arrayPalavra + "<br> Tentativas = " + tentativas + "<br> Letras erradas: " + letrasErradas
+  exibeImagem()
   if (tentativas == 0) {
     alert("Você perdeu... A palavra era " + palavraSorteada)
     location.reload()
