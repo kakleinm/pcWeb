8583d60e132bcb1316c51e935bd319b5d695d98a
Karen KM <karenkmaia09@gmail.com>
2025-08-29 18:16:04 -0300

    cafe

---
+++ cafet/script.js
@@ -0,0 +1,37 @@
+const itens = [
+  {
+    "nome": "Café Espresso",
+    "preco": 8.00,
+    "img": "https://uploads.metroimg.com/wp-content/uploads/2019/10/17184313/190115JB-Koppe-cafes-especiais-0121.jpg"
+  },
+  {
+    "nome": "Capuccino",
+    "preco": 12.00,
+    "img": "https://www.minhareceita.com.br/app/uploads/2024/07/mobile-capuccino.jpg"
+  },
+  {
+    "nome": "Suco de Laranja",
+    "preco": 10.00,
+    "img": "https://cardapio.primeirobar.com.br/wp-content/uploads/2022/04/laranja.jpg"
+  },
+  {
+    "nome": "Água mineral",
+    "preco": 6.00,
+    "img": "https://fontagua.com.br/wp-content/uploads/2019/02/splash_350ml.jpg"
+  },
+  {
+    "nome": "Fatia de bolo",
+    "preco": 12.00,
+    "img": "https://cdn.awsli.com.br/2500x2500/2658/2658919/produto/257217444/fatia-de-bolo-vegano-de-brigadeiro-com-morangos-sem-gl-ten-fnv2j0u5hp.png"
+  },
+  {
+    "nome": "Sanduíche natural",
+    "preco": 15.00,
+    "img": "https://guiadacozinha.com.br/wp-content/uploads/2014/01/Sanduiche-natural-768x619.jpg"
+  },
+  {
+    "nome": "Croissant",
+    "preco": 17.00,
+    "img": "https://www.fornodeminas.com.br/wp-content/uploads/2018/06/croissant.jpg"
+  }
+]
